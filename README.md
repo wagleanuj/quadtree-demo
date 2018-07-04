00c1ae9b0fd0ff18e3fea713715616aafcdf41ed
Oyed <tom@mosey.io>
2018-07-04 16:41:55 +0100

    Added a second demo and more stats.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Quadtree Demo
+
+A demo to showcase my [Quadtree JS Library](https://github.com/oyed/quadtree).
+
+## Usage
+
+There are two demos here - one using a traditional, simple method of checking collisions, and one with the Quadtree. You can find them here:
+
+- [Traditional Collision checking](https://quadtree.oyed.io/#bad)
+- [Quadtree Collision checking](https://quadtree.oyed.io/)
--- src/states/generate.js
+++ src/states/generate.js
@@ -1,7 +1,8 @@
-import { Graphics, Container } from 'pixi.js';
-import { rand } from 'helpers';
+import { Graphics, Text, Container } from 'pixi.js';
+import { rand, rectIntersects } from 'helpers';
 import State from 'states/state';
 import Quadtree from '@oyed/quadtree';
+import Debug from 'debug';
 
 class Generate extends State {
 
@@ -11,15 +12,47 @@ class Generate extends State {
     constructor() {
         super('generate');
 
+        this.bad = window.location.hash.substr(1) === 'bad';
+
+        this.mapWidth = 800;
+        this.mapHeight = 800;
+        this.entityWidth = 10;
+        this.entityHeight = 10;
+
         this.entities = [];
+
         this.tree = new Quadtree({
-            width: 500,
-            height: 500,
+            width: this.mapWidth,
+            height: this.mapHeight,
             max_depth: 5,
             max_objects: 3,
         });
+
+        this.game = new Container();
+        this.addChild(this.game);
+
         this.debug = new Graphics();
         this.addChild(this.debug);
+
+        this.fps = new Text('-', {
+            fill: 0x000000,
+            fontWeight: 'bold',
+            fontSize: 18,
+        });
+        this.fps.x = 10;
+        this.fps.y = 10;
+        this.addChild(this.fps);
+
+        this.checks = new Text('-', {
+            fill: 0x000000,
+            fontWeight: 'bold',
+            fontSize: 18,
+        });
+        this.checks.x = 10;
+        this.checks.y = 30;
+        this.addChild(this.checks);
+
+        Debug.on('fps', fps => this.fps.text = `${fps} FPS`);
     }
 
     /**
@@ -30,24 +63,31 @@ class Generate extends State {
     up(options) {
         super.up(options);
 
-        const width = 10;
-        const height = 10;
-
-        for (let i = 0; i < 50; i++) {
+        for (let i = 0; i < 500; i++) {
             const entity = new Container();
-            const g = new Graphics();
-            g.beginFill(0xFF0000);
-            g.drawRect(0, 0, width, height);
-            g.endFill();
-            entity.x = rand(0, 500 - width);
-            entity.y = rand(0, 500 - height);
+            entity.highlighted = new Graphics();
+            entity.default = new Graphics();
+
+            entity.default.beginFill(0xFF0000);
+            entity.default.drawRect(0, 0, this.entityWidth, this.entityHeight);
+            entity.default.endFill();
+
+            entity.highlighted.beginFill(0x00FF00);
+            entity.highlighted.drawRect(0, 0, this.entityWidth, this.entityHeight);
+            entity.highlighted.endFill();
+            entity.highlighted.visible = false;
+
+            entity.x = rand(0, this.mapWidth - this.entityWidth);
+            entity.y = rand(0, this.mapHeight - this.entityHeight);
             entity.velocity = {
                 x: rand(0, 1) === 0 ? (rand(0, 1) === 0 ? 1 : -1) : 0,
                 y: rand(0, 1) === 0 ? (rand(0, 1) === 0 ? 1 : -1) : 0,
             };
-            entity.g = g;
-            entity.addChild(g);
-            this.addChild(entity);
+
+            entity.addChild(entity.default);
+            entity.addChild(entity.highlighted);
+
+            this.game.addChild(entity);
             this.entities[i] = entity;
         }
     }
@@ -61,10 +101,16 @@ class Generate extends State {
         const { entities } = this;
         const total = entities.length;
 
-        this.tree.clear();
+        if (!this.bad) {
+            this.tree.clear();
+        }
 
         for (let e = 0; e < total; e++) {
             const entity = entities[e];
+
+            if (entity.highlighted.visible) {
+                entity.highlighted.visible = false;
+            }
 
             entity.x += entity.velocity.x;
             entity.y += entity.velocity.y;
@@ -103,44 +149,85 @@ class Generate extends State {
                 entity.y = 0;
             }
 
-            if (entity.x + entity.width > 500) {
-                entity.x = 500 - entity.width;
+            if (entity.x + entity.width > this.mapWidth) {
+                entity.x = this.mapWidth - entity.width;
             }
 
-            if (entity.y + entity.height > 500) {
-                entity.y = 500 - entity.height;
+            if (entity.y + entity.height > this.mapHeight) {
+                entity.y = this.mapHeight - entity.height;
             }
 
-            this.tree.insert({
-                x: entity.x,
-                y: entity.y,
-                width: entity.width,
-                height: entity.height,
-                index: e,
-            });
+            if (!this.bad) {
+                this.tree.insert({
+                    x: entity.x,
+                    y: entity.y,
+                    width: entity.width,
+                    height: entity.height,
+                    index: e,
+                });
+            }
         }
 
-        this.debug.clear();
-        this.debug.lineStyle(1, 0x0000FF, 1);
+        let checks = 0;
 
-        const renderNode = node => {
-            if (node.parent) {
-                for (let i = 0; i < 4; i++) {
-                    renderNode(node.children[i]);
+        for (let e = 0; e < total; e++) {
+            const entity = entities[e];
+
+            if (this.bad) {
+                for (let e2 = 0; e2 < total; e2++) {
+                    if (e !== e2) {
+                        checks++;
+
+                        if (rectIntersects(entity, this.entities[e2])) {
+                            entity.highlighted.visible = true;
+                            this.entities[e2].highlighted.visible = true;
+                        }
+                    }
+                }
+            } else {
+                const potential = this.tree.retrieve(entity);
+                const totalPotential = potential.length;
+
+                for (let p = 0; p < totalPotential; p++) {
+                    const index = potential[p].index;
+
+                    if (e !== index) {
+                        checks++;
+
+                        if (rectIntersects(entity, this.entities[index])) {
+                            entity.highlighted.visible = true;
+                            this.entities[index].highlighted.visible = true;
+                        }
+                    }
                 }
             }
+        }
 
-            const { options } = node;
+        this.checks.text = `${checks} Collision Checks`;
 
-            this.debug.moveTo(options.x, options.y);
-            this.debug.lineTo(options.x, options.y + options.height);
-            this.debug.lineTo(options.x + options.width, options.y + options.height);
-            this.debug.lineTo(options.x + options.width, options.y);
-            this.debug.lineTo(options.x, options.y);
-            this.debug.endFill();
-        };
+        if (!this.bad) {
+            this.debug.clear();
+            this.debug.lineStyle(1, 0x0000FF, 1);
 
-        renderNode(this.tree);
+            const renderNode = node => {
+                if (node.parent) {
+                    for (let i = 0; i < 4; i++) {
+                        renderNode(node.children[i]);
+                    }
+                }
+
+                const { options } = node;
+
+                this.debug.moveTo(options.x, options.y);
+                this.debug.lineTo(options.x, options.y + options.height);
+                this.debug.lineTo(options.x + options.width, options.y + options.height);
+                this.debug.lineTo(options.x + options.width, options.y);
+                this.debug.lineTo(options.x, options.y);
+                this.debug.endFill();
+            };
+
+            renderNode(this.tree);
+        }
     }
 
 }
